d83278a3ec2685ee5e2adccc4d9f58ccfe16a09d
Nereh <noe.balli@yahoo.com>
2025-03-26 21:33:33 -0500

    working board ui

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,6 +1,6 @@
 from cellitaire.environment.cellitaire_env import CellitaireEnv
 from cellitaire.game.game import Game
-from cellitaire.environment.rewards.reward import get_stockpile_cards, get_cards_in_foundation
+from cellitaire.environment.rewards.reward import get_stockpile_cards, get_cards_in_foundation, get_available_moves_count, ConstantReward
 
 def test_init_doesnt_crash():
   env = CellitaireEnv(None)
@@ -10,12 +10,14 @@ def test_state_translator():
   rows = 7
   cols = 12
   num_reserved = 6
-  env = CellitaireEnv(None, rows=rows, cols=cols, num_reserved=num_reserved)
+  env = CellitaireEnv(ConstantReward(), num_reserved=num_reserved)
   env.reset()
   state = env.get_state()
 
-  actual_stockpile_cards = get_stockpile_cards(state, rows, cols, num_reserved)
-  actual_cards_in_foundation = get_cards_in_foundation(state, rows, cols)
+  actual_stockpile_cards = get_stockpile_cards(state)
+  actual_cards_in_foundation = get_cards_in_foundation(state)
+  actual_moves = get_available_moves_count(state)
 
   assert env.game.foundation.total_cards() == actual_cards_in_foundation
   assert env.game.stockpile.count() == actual_stockpile_cards
+  assert env.legal_actions_count() == actual_moves
